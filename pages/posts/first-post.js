018d86d75c88c9d0acb70541b0d75193dc020d0e
111hyeok <wonhyeok0823@gmail.com>
2024-04-12 17:49:25 +0900

    add other page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Hello My name is Won Hyeok Chang. I'm from Korea</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
